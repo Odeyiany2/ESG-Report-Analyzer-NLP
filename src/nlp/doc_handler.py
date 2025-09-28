578822ae68c6d4fe2d82dc9eaa8ebcca9a1e0d2b
Odeyiany2 <miriamodeyianypeter@gmail.com>
2025-09-28 23:07:12 +0100

    Updated imports

--- src/nlp/doc_handler.py
+++ src/nlp/doc_handler.py
@@ -1,8 +1,9 @@
 import os
 from pathlib import Path 
 from typing import List
-from langchain.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
 
 
-def load_uploaded_documents(upload_dir: str):
+def load_uploaded_documents(upload_dir: str) -> List[Document]:
     pass
