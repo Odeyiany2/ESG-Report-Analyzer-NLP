cb5e057e9faf990e68ab5d7cc3b859b8d90f17b6
Odeyiany2 <miriamodeyianypeter@gmail.com>
2025-10-06 20:39:30 +0100

    Added embedding function for standards

--- src/nlp/embeddings.py
+++ src/nlp/embeddings.py
@@ -3,3 +3,6 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_core.documents import Document
+
+def embed_standards(standards: List[str], persist_directory: str) -> Chroma:
+    pass
