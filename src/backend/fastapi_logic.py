40ebd91636e455a3332fb782bd9698b03b23b5ca
Odeyiany2 <miriamodeyianypeter@gmail.com>
2025-10-15 20:41:38 +0100

    Updated the predict ESG score endpoint

--- src/backend/fastapi_logic.py
+++ src/backend/fastapi_logic.py
@@ -117,10 +117,10 @@ async def query_assistant(request:Request):
 
 
 
-@app.post("/regression_analysis")
-async def regression_analysis():
+@app.post("/predict_esg_score")
+async def predict_esg_score():
     pass
 
-@app.post("/forecast_esg_trends")
-async def forecast_esg_trends():
-    pass
+# @app.post("/forecast_esg_trends")
+# async def forecast_esg_trends():
+#     pass
