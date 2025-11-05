7f0f3111f5810579bdf64da1bd64ecd37f0f42ce
Odeyiany2 <miriamodeyianypeter@gmail.com>
2025-11-05 22:04:57 +0100

    Updated with the structure and logic for the agentic node

--- src/agentic_ai/orchestrator.py
+++ src/agentic_ai/orchestrator.py
@@ -4,4 +4,34 @@ from langgraph.graph import StateGraph, END, START
 from langgraph.prebuilt import tools_condition, ToolNode
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import MemorySaver
-from typing import Annotated, TypedDict
+from typing import Annotated, Any, Optional, Dict, List
+from pydantic import BaseModel, Field
+
+retriever = Retriever()
+
+#define the agent state
+class ESGstate(BaseModel):
+    query: str
+    retrieved_docs: Optional[Dict[str, List]] = None
+    classification: Optional[Dict[str, Any]] = None
+    scores: Optional[Dict[str, float]] = None
+    response: Optional[str] = None
+    messages: Annotated[list[dict], add_messages]
+
+#retrieve ESG report node
+def retrieve_node(state):
+    query = state.query
+    esg_agents_logger.info(f"Retrieving documents for query: {query}")
+
+
+#classify ESG aspects node
+def classify_node(state):
+    pass
+
+#ESG scores node 
+def predict_node(state):
+    pass
+
+#summarize ESG findings node
+def summarize_node(state):
+    pass
